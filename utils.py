943913539d984afdcfa1dc95be59e0484c973592
draymone <socotcho@gmail.com>
2023-04-15 20:55:49 +0200

    Some ident corrections

--- utils.py
+++ utils.py
@@ -9,14 +9,14 @@ def convert_for_polybe(text):
     i = 0
     while i < len(text):  # Replace W by VV
         if text[i] == "W":
-            text = text[:i] + "VV" + text[i+1:len(text)]
+            text = text[:i] + "VV" + text[i + 1:len(text)]
         i += 1
 
     i = 0
     uppercases = list(string.ascii_uppercase)
     while i < len(text):  # Remove non numeric characters
         if not text[i] in uppercases:
-            text = text[:i] + text[i+1:len(text)]
+            text = text[:i] + text[i + 1:len(text)]
             i -= 1
         i += 1
 
@@ -33,7 +33,7 @@ def get_polybius_square():
     for i in range(5):
         sublist = []
         for j in range(5):
-            sublist += letters[5*i + j]
+            sublist += letters[5 * i + j]
         polybius_square[i] = sublist
 
     return polybius_square
